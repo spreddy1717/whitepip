bfd882ceaa74dae5945752c449fdaa865b29d3c2
Sai Prasanna Kumar Reddy <spreddy1717@gmail.com>
2023-08-08 18:42:10 +0530

    work in progress

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,6 @@ from . import views
 from .views import BookCreateView
 
 urlpatterns = [
-    path("/", views.books, name="books"),
+    path("/", views.Book, name="Book"),
     path("api/books/", BookCreateView.as_view(), name="create-book"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,12 +1,4 @@
 from django.http import HttpResponse
-from django.template import loader
-
-
-def books(request):
-    template = loader.get_template("myfirst.html")
-    return HttpResponse(template.render())
-
-
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
