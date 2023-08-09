e139235249143c8d7b6c83bb30fed8d11531ee38
Sai Prasanna Kumar Reddy <spreddy1717@gmail.com>
2023-08-09 17:32:05 +0530

    Add GET method for books

--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,6 @@ from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path("api/books/", views.BookList.as_view()),
+    path("", views.BooksView.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- books/views.py
+++ books/views.py
@@ -1,23 +1,20 @@
-from .models import Book
-from .serializers import BookSerializer
+# books/views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Book
+from .serializers import BookSerializer
 
 
-class BookList(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
-
-    def get(self, request, format=None):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
-
+class BooksView(APIView):
     def post(self, request, format=None):
         serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, format=None):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
