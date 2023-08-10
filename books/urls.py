c11e60be5a74631aff1e32fb24220b2caa2b759c
Sai Prasanna Kumar Reddy <spreddy1717@gmail.com>
2023-08-10 12:42:01 +0530

    woking books folder

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,33 @@
 from django.urls import path
-from . import views
+from .views import BookCreateView, BookListView, BookDeleteView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="books API",
+        default_version="v1",
+        description="API for managing books",
+        terms_of_service="http://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@wxample.com"),
+        license=openapi.License(name="example Licence"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path("", views.BooksView.as_view()),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("api/books/", BookCreateView.as_view(), name="book-create"),
+    path("api/books/list/", BookListView.as_view(), name="book-list"),
+    path("api/books/<uuid:uuid>/", BookDeleteView.as_view(), name="book-delete"),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- books/views.py
+++ books/views.py
@@ -1,12 +1,12 @@
-# books/views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Book
+from .models import Author, Book
 from .serializers import BookSerializer
+from django.shortcuts import get_object_or_404
 
 
-class BooksView(APIView):
+class BookCreateView(APIView):
     def post(self, request, format=None):
         serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
@@ -14,7 +14,16 @@ class BooksView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class BookListView(APIView):
     def get(self, request, format=None):
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class BookDeleteView(APIView):
+    def delete(self, request, uuid, format=None):
+        book = get_object_or_404(Book, uuid=uuid)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
