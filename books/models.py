3bab3927ca6635358098566f073bf5008f219b41
Sai Prasanna Kumar Reddy <spreddy1717@gmail.com>
2023-08-14 14:06:53 +0530

    corrections addressed

--- books/models.py
+++ books/models.py
@@ -1,29 +1,40 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    uuid = models.UUIDField(primary_key=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     biography = models.TextField(blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to="profile/", blank=True, null=True)
 
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.uuid = f"author_{uuid.uuid4()}"
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.name
-
+    
 
 class Category(models.Model):
-    uuid = models.UUIDField(primary_key=True)
-    category_name = models.CharField(max_length=50)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    categeory_name = models.CharField(max_length=50)
     description = models.TextField(max_length=1000)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.uuid = f"Category_{uuid.uuid4()}"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.Categeoryname
-
+    
 
 class Book(models.Model):
-    uuid = models.UUIDField(primary_key=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=1000)
     isbn = models.CharField(max_length=13, unique=True)
     summary = models.TextField()
@@ -33,6 +44,11 @@ class Book(models.Model):
     category = models.ManyToManyField(Category)
     author = models.ManyToManyField(Author)
     stock = models.PositiveBigIntegerField()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.uuid = f"Book_{uuid.uuid4()}"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
