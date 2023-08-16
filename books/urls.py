e897b905a1a8d4b8bd8164199ad1c87c6e86d01f
Sai Prasanna Kumar Reddy <spreddy1717@gmail.com>
2023-08-16 14:36:58 +0530

    black formatted urls.py

--- books/urls.py
+++ books/urls.py
@@ -34,16 +34,32 @@ schema_view = get_schema_view(
 app_name = "books"
 
 urlpatterns = [
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path("api/books/", BookCreateView.as_view(), name="book-create"),
     path("api/books/list/", BookListView.as_view(), name="book-list"),
-    path("api/books/<str:pk>/", BookGetUpdateDeleteView.as_view(), name="book-get-update-delete"),
+    path(
+        "api/books/<str:pk>/",
+        BookGetUpdateDeleteView.as_view(),
+        name="book-get-update-delete",
+    ),
     path("api/authors/", AuthorCreateView.as_view(), name="author-create"),
     path("api/authors/list/", AuthorListView.as_view(), name="author-list"),
-    path("api/authors/<str:pk>/", AuthorGetUpdateDeleteView.as_view(), name="author-get-update-delete"),
+    path(
+        "api/authors/<str:pk>/",
+        AuthorGetUpdateDeleteView.as_view(),
+        name="author-get-update-delete",
+    ),
     path("api/categories/", CategoryCreateView.as_view(), name="category-create"),
     path("api/categories/list/", CategoryListView.as_view(), name="category-list"),
-    path("api/categories/<str:pk>/", CategoryGetUpdateDeleteView.as_view(), name="category-get-update-delete"),
+    path(
+        "api/categories/<str:pk>/",
+        CategoryGetUpdateDeleteView.as_view(),
+        name="category-get-update-delete",
+    ),
 ]
 
 
